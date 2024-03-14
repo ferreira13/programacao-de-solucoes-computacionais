9ad28f2071be8b81a0ead26f7d0d7fae9ce29b97
Atila Ferreira <atila.ferreira13@icloud.com>
2024-03-13 21:37:56 -0300

    setup basic architecture

--- pratique/src/User.java
+++ pratique/src/User.java
@@ -1,7 +1,7 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.ArrayList;
 import java.util.List;
+// import java.util.regex.Matcher;
+// import java.util.regex.Pattern;
 
 public class User {
     private String email;
@@ -11,48 +11,54 @@ public class User {
     private String password;
     private List<String> events;
 
-    private static List<String> emailList = new ArrayList<>();
-    private static List<String> cpfList = new ArrayList<>();
-
     public User(String email, String name, String cpf, String password, String birthday) {
-        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
-        String cpfRegex = "^(?:(?!000\\.?000\\.?000-?00).)*$|^\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}$";
-        String birthdayRegex = "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4}$";
-
-        Pattern emailPattern = Pattern.compile(emailRegex);
-        Matcher emailMatcher = emailPattern.matcher(email);
-        Pattern cpfPattern = Pattern.compile(cpfRegex);
-        Matcher cpfMatcher = cpfPattern.matcher(cpf);
-        Pattern birthdayPattern = Pattern.compile(birthdayRegex);
-        Matcher birthdayMatcher = birthdayPattern.matcher(birthday);
-
-        Boolean emailIsOk = emailMatcher.matches();
-        Boolean nameIsOk = name.length() > 0;
-        Boolean cpfIsOk = cpfMatcher.matches();
-        Boolean birthdayIsOk = birthdayMatcher.matches();
-        Boolean passwordIsOk = password.length() > 8;
-
-        if (!emailIsOk) {
-            throw new IllegalArgumentException("Por favor, verifique se o e-mail fornecido está correto");
-        }
-        if (!nameIsOk) {
-            throw new IllegalArgumentException("Por favor, verifique se o Nome fornecido está correto");
-        }
-        if (!cpfIsOk) {
-            throw new IllegalArgumentException("Por favor, verifique se o CPF fornecido está correto");
-        }
-        if (!passwordIsOk) {
-            throw new IllegalArgumentException("Por favor, verifique se a senha fornecida está correta");
-        }
-        if (!birthdayIsOk) {
-            throw new IllegalArgumentException("Por favor, verifique se a data de nascimento fornecida está correta");
-        }
-        if (emailList.contains(email)) {
-            throw new IllegalArgumentException("email ja cadastrado");
-        }
-        if (cpfList.contains(cpf)) {
-            throw new IllegalArgumentException("cpf ja cadastrado");
-        }
+        /*
+         * String emailRegex =
+         * "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+         * String cpfRegex =
+         * "^(?:(?!000\\.?000\\.?000-?00).)*$|^\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}$";
+         * String birthdayRegex = "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4}$";
+         * 
+         * Pattern emailPattern = Pattern.compile(emailRegex);
+         * Matcher emailMatcher = emailPattern.matcher(email);
+         * Pattern cpfPattern = Pattern.compile(cpfRegex);
+         * Matcher cpfMatcher = cpfPattern.matcher(cpf);
+         * Pattern birthdayPattern = Pattern.compile(birthdayRegex);
+         * Matcher birthdayMatcher = birthdayPattern.matcher(birthday);
+         * 
+         * Boolean emailIsOk = emailMatcher.matches();
+         * Boolean nameIsOk = name.length() > 0;
+         * Boolean cpfIsOk = cpfMatcher.matches();
+         * Boolean birthdayIsOk = birthdayMatcher.matches();
+         * Boolean passwordIsOk = password.length() > 8;
+         * 
+         * if (!emailIsOk) {
+         * throw new IllegalArgumentException("Por favor, verifique se o e-mail
+         * fornecido está correto");
+         * }
+         * if (!nameIsOk) {
+         * throw new IllegalArgumentException("Por favor, verifique se o Nome fornecido
+         * está correto");
+         * }
+         * if (!cpfIsOk) {
+         * throw new IllegalArgumentException("Por favor, verifique se o CPF fornecido
+         * está correto");
+         * }
+         * if (!passwordIsOk) {
+         * throw new IllegalArgumentException("Por favor, verifique se a senha fornecida
+         * está correta");
+         * }
+         * if (!birthdayIsOk) {
+         * throw new IllegalArgumentException("Por favor, verifique se a data de
+         * nascimento fornecida está correta");
+         * }
+         * if (emailList.contains(email)) {
+         * throw new IllegalArgumentException("email ja cadastrado");
+         * }
+         * if (cpfList.contains(cpf)) {
+         * throw new IllegalArgumentException("cpf ja cadastrado");
+         * }
+         */
 
         this.email = email;
         this.name = name;
@@ -60,10 +66,6 @@ public class User {
         this.password = password;
         this.birthday = birthday;
         this.events = new ArrayList<>();
-
-        emailList.add(email);
-        cpfList.add(cpf);
-
     }
 
     // getters
--- pratique/src/services/AppService.java
+++ pratique/src/services/AppService.java
@@ -1,7 +1,6 @@
-public class App {
+public class AppService {
     public static void main(String[] args) throws Exception {
         var nome = "Atila";
         System.out.println(String.format("My name is %s and i say 'Hello World!'", nome));
     }
 }
-
